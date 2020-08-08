d8e1bdb675ea9f248ad86ea1f34fb4351d52e22a
Marcin Polonis <marcin.polonis@o2.pl>
2020-08-08 17:53:55 +0200

    Start project about refs and portal

---
+++ refs/src/App.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {Editor} from "./Editor"
+import {ProductTable} from "./ProductTable";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+    }
+  }
+
+  addProduct = (product) => {
+    if (this.state.products.indexOf(product.name) === -1) {
+      this.setState({products: [...this.state.products, product]});
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Editor callback={this.addProduct} />
+        <h6 className="bg-secondary text-white m-2 p-2">Produkty</h6>
+        <div className="m-2">
+          {
+            this.state.products.length === 0 
+            ? <div className="text-center">Brak produktów</div>
+            : <ProductTable products = {this.state.products} />
+          }
+        </div>
+      </div>
+    )
+  }
+}
